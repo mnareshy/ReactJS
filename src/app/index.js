115a2fdb7e516e8aba6b6c8813c66ba2c7eab391
Naresh M <mnaresh.y@gmai.com>
2017-04-10 09:43:55 +0530

    updated index.js and index.html

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,33 @@
+import React from "react";
+import { render } from "react-dom"
+
+class SampleApp extends React.Component{
+	
+	render(){
+		
+
+	 return(
+		
+		<div className="container">
+		
+			<div className="row">
+			
+				<div className="col-xs-10 col-xs-offset-1">
+				
+				</div>
+			
+			
+			</div>
+			
+			<h1>Sample App - SaiRam </h1>
+		
+		</div>
+		);
+	}
+}
+
+render(<SampleApp/>, window.document.getElementById("sampleapp"));
+ 
 console.log("test - sairam")
+
+
